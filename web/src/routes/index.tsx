ab87145884b5a0fcb5c2a14e164021f70b96c6a6
agpprastyo <prasetyo.agpr@gmail.com>
2026-01-30 22:00:57 +0700

    add

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,6 +1,10 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
 
+
+console.log("API BASE =", import.meta.env.VITE_API_BASE)
 export const Route = createFileRoute('/')({
+
+
   beforeLoad: () => {
     throw redirect({
       to: '/$locale',

c1349876ee3a0fc44355a20e2f8d5c1598c74200
agpprastyo <prasetyo.agpr@gmail.com>
2026-01-31 13:49:22 +0700

    Remove Cloudflare Vite plugin and refactor SPA prerender configuration within `tanstackStart`.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,30 +4,26 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from "@cloudflare/vite-plugin";
+
 
 
 const config = defineConfig({
   base: '/',
   plugins: [
-
     devtools(),
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
     tanstackStart({
-     
       spa: {
         enabled: true,
+        prerender: {
+          enabled: false,
+        },
       },
-      prerender: {
-        enabled: false,
-      },
-     
     }),
     viteReact(),
-    cloudflare()
 
   ],
 })
